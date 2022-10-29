8d2e8c9b87228130f674f162afa4ee0e61c4918e
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-10-29 21:23:01 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'F', 'black', 'temp', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 22, 'M', 'grey', 'temp', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'F', 'black', 'temp', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 17, 'M', 'black', 'temp', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'M', 'black', 'temp', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 10, 'F', 'blue', 'temp', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'M', 'black', 'temp', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'brown', 'temp', 0);
